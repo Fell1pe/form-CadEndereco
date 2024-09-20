b672dc7e5605010fd40cbb4d1e78e7089ce7f3e8
Fell1pe <fellipe.lucas.zanin@gmail.com>
2024-09-20 07:32:10 -0300

    adiciona campo de validação do cep e também adiciona preenchimento de campos no formulário

--- main.js
+++ main.js
@@ -13,6 +13,20 @@ const limparFormlario = () =>{
 } 
 
 // Verifica se o CEP é valido
+// faz uma expreção regular para verificar se apenas foram digitados números
 const eNumero = (Numero) => /^[0-9]+$/.test(Numero);
 // Verifica o tamanho do cep 
+// Cria um const puxando o id de cep e limitando seu tamanho a oito caractéres
 const cepValido = (cep) => cep.lengt == 8 && eNumero(cep);
+
+//Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('Rua').value = endereco.logradouro;
+    //coloca o valor de logradouro da API dentro do campo Rua do formulário
+    document.getElementById('Bairro').value = endereco.bairro;
+    //coloca o valor de Bairro da API dentro do campo Bairro do formulário
+    document.getElementById('Cidade').value = endereco.localidade;
+    //coloca o valor de localidade da API dentro do campo Cidade do formulário
+    document.getElementById('Estado').value = endereco.uf;
+    //coloca o valor de uf da API dentro do campo Estado do formulário
+}
