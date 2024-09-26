bf020acba4a516d29497f9e127ffffca8b0a62bf
Fell1pe <fellipe.lucas.zanin@gmail.com>
2024-09-26 19:32:51 -0300

    adiciona const para a verficação do CEP

--- main.js
+++ main.js
@@ -6,27 +6,48 @@
 // LIMPAR CONSULTA DO FORM JA REALIZADO
 
 const limparFormlario = () =>{
-    document.getElementById('Rua').value = '';
-    document.getElementById('Cidade').value = '';
-    document.getElementById('Bairro').value = '';
-    document.getElementById('Estado').value = '';
+    document.getElementById('rua').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('estado').value = '';
 } 
-
-// Verifica se o CEP é valido
-// faz uma expreção regular para verificar se apenas foram digitados números
-const eNumero = (Numero) => /^[0-9]+$/.test(Numero);
-// Verifica o tamanho do cep 
-// Cria um const puxando o id de cep e limitando seu tamanho a oito caractéres
-const cepValido = (cep) => cep.lengt == 8 && eNumero(cep);
 
 //Função para preencher campos relacionados ao CEP
 const preencherFormulario = (endereco) =>{
-    document.getElementById('Rua').value = endereco.logradouro;
+    document.getElementById('rua').value = endereco.logradouro;
     //coloca o valor de logradouro da API dentro do campo Rua do formulário
-    document.getElementById('Bairro').value = endereco.bairro;
+    document.getElementById('bairro').value = endereco.bairro;
     //coloca o valor de Bairro da API dentro do campo Bairro do formulário
-    document.getElementById('Cidade').value = endereco.localidade;
+    document.getElementById('cidade').value = endereco.localidade;
     //coloca o valor de localidade da API dentro do campo Cidade do formulário
-    document.getElementById('Estado').value = endereco.uf;
+    document.getElementById('estado').value = endereco.uf;
     //coloca o valor de uf da API dentro do campo Estado do formulário
 }
+
+// Verifica se o CEP é valido
+// faz uma expreção regular para verificar se apenas foram digitados números
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+// Verifica o tamanho do cep 
+// Cria um const puxando o id de cep e limitando seu tamanho a oito caractéres
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+
+const pesquisarCep =async() => {
+    limparFormlario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+
+    if(addres.hasOwnProperty('erro')){
+        alert('CEP não encontado')  
+    }
+    else{
+        preencherFormulario(addres);
+    }
+}else{
+        alert('CEP incorreto')
+    }
+    } 
+// Executa a ação de preenchimento do formulário ao deixar o campo CEP
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
